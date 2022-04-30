6a64163abb3fe5b54605e1be9fd9eedd03596616
carloseur24 <carloseur24@gmail.com>
2022-04-30 14:12:51 -0400

    adding final sets

--- Save-Capacity/savings.js
+++ Save-Capacity/savings.js
@@ -13,14 +13,16 @@ let done = 0;
 const getAdvice = () => {
     const table = document.createElement('table');
     const p2 = document.createElement('p')
-    text = document.createTextNode('Some tips to save better')
+    const p3 = document.createElement('p')
+    text = document.createTextNode('Some tips to save better') 
     p2.appendChild(text)
     const advice = advices.map((i) => {
         const row1 = document.createElement('tr');
         text = document.createTextNode('#' + i)
         section2.appendChild(p2)
-        p2.appendChild(table)
-        p2.setAttribute('class', 'c1')
+        section2.appendChild(p3)
+        p2.setAttribute('class', 'tips')
+        p3.appendChild(table), p3.setAttribute('class', 'tips2')
         table.appendChild(row1)
         row1.appendChild(text)
     })
