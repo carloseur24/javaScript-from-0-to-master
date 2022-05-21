9f50d0bf48a21fa25c109a75614f55ca54deaf94
klinker <carloseur24@gmail.com>
2022-05-20 23:28:23 -0400

    geometrical figures.js ended

--- public/WorkShop-01/geometrical-figures.js
+++ public/WorkShop-01/geometrical-figures.js
@@ -1,8 +1,8 @@
 // perímetro del cuadrado = b*4
 // área del cuadrado = l*l
 // ====================
-// área del triángulo = l+l+l
-// perímetro del triíngulo = (b*a)/2
+// perimeter del triangule = l+l+l
+// area del triangule = (b*a)/2
 // ======================
 // perímetro del circulo = diámetro * PI
 // área de un circulo = radio *radio * PI
@@ -11,49 +11,83 @@
 const calculatePerimeterSquare = () => {
     const input = document.getElementById('InputSquare')
     const value = input.value;
-    const perimeter = sperimeter(value);
-    validate(value, perimeter, "#i1");
+    const value2 = value.split(" ")
+    const perimeter = sperimeter(value2);
+    if (perimeter) {
+        if (typeof perimeter === 'object' &&
+            !Array.isArray(perimeter) &&
+            perimeter !== null) {
+            alersweet2()
+        } else {
+            validate(value2, perimeter, "#i1");
+        }
+    }
 }
 const calculateAreaSquare = () => {
     const input = document.getElementById('InputSquare')
     const value = input.value;
-    const area = areasquare(value);
-    validate(value, area, "#i1");
+    const value2 = value.split(" ")
+    const area = areasquare(value2);
+    if (area) {
+        if (typeof area === 'object' &&
+            !Array.isArray(area) &&
+            area !== null) {
+            alersweet2()
+        } else {
+            validate(value2, area, "#i1");
+        }
+    }
 }
-const calculateAreaTriangle = () => {
-    const inputa = document.getElementById('InputTrianglea')
-    const inputb = document.getElementById('InputTriangleb')
-    const inputc = document.getElementById('InputTrianglec')
-    const valuea = parseInt(inputa.value);
-    const valueb = parseInt(inputb.value);
-    const valuec = parseInt(inputc.value);
-    const area = tarea(valuea, valueb, valuec);
-    validate(valuea, area, "#i2",valueb);
-}
-
 const calculatePerimeterTriangle = () => {
-    const inputa = document.getElementById('InputTrianglea')
-    const inputb = document.getElementById('InputTriangleb')
-    const inputc = document.getElementById('InputTrianglec')
-    const valuea = parseInt(inputa.value);
-    const valueb = parseInt(inputb.value);
-    const valuec = parseInt(inputc.value);
-    const height = theight(valuea, valueb, valuec)
-    const perimeter = tperimeter(valuec, height);
-    validate(valuea, perimeter, "#i2",valueb);
+    const input = document.getElementById('InputTrianglea')
+    const value = input.value;
+    const value1 = value.split(" ");
+    const l = value1.length
+    console.log(value1)
+    if (l >= 4) {
+        alersweet1()
+    } else if (l == 2) {
+        alersweet1()
+    } else if (value1.length == 1) {
+        const area = tperimeter(parseInt(value1[0]), parseInt(value1[0]), parseInt(value1[0]));
+        validate(value1, area, "#i2");
+    } else {
+        const area = tperimeter(parseInt(value1[0]), parseInt(value1[1]), parseInt(value1[2]));
+        console.log(value1)
+        validate(value1, area, "#i2");
+    }
+} 
+const calculateAreaTriangle = () => {
+    const input = document.getElementById('InputTrianglea')
+    const value = input.value;
+    const value1 = value.split(" ");
+    const l = value1.length
+    if (l >= 4) {
+        alersweet1()
+    } else if (l == 2) {
+        const result = area2(parseInt(value1[0]), parseInt(value1[1]));
+        validate(value1, result, "#i2");
+    } else if (l == 1) {
+        const result = area3(parseInt(value1[0]), parseInt(value1[0]), parseInt(value1[0]));
+        validate(value1, result, "#i2");
+    } else {
+        const result = area3(parseInt(value1[0]), parseInt(value1[1]), parseInt(value1[2]));
+        validate(value1, result, "#i2");
+    }
 }
 const calculatePerimeterCircle = () => {
     const input = document.getElementById('InputCircle')
     const button = document.querySelector(".circlea");
     const value = input.value;
+    const value1 = [value]
     const perimeter = cperimeter(value);
-    validate(value, perimeter, "#i3");
+    validate(value1, perimeter, "#i3");
 }
 const calculateAreaCircle = () => {
     const input = document.querySelector("#InputCircle");
     const button = document.querySelector(".circleb");
     const value = input.value;
+    const value1 = [value]
     const area = carea(value);
-    validate(value, area, "#i3");
+    validate(value1, area, "#i3");
 }
-
--- public/WorkShop-01/interaction.js
+++ public/WorkShop-01/interaction.js
@@ -1,50 +1,118 @@
+const alersweet1 = () => {
+    return swal(`Value don't permitted`, {
+        button: ["OK"]
+    });
+}
+const alersweet2 = () => {
+    return swal(`An Square has all equal sides`, {
+        button: ["OK"]
+    });
+}
 const sperimeter = (side) => {
-    return side * 4 + ' cm';
+    if (side.length == 1) {
+        return parseInt(side[0]) * 4 + ' cm';
+    } else if (side.length > 1 && side.length < 4) {
+        alersweet1()
+    } else if (side[0] !== side[3]) {
+        alersweet2()
+    } else {
+        return (parseInt(side[0]) + parseInt(side[1]) + parseInt(side[2]) + parseInt(side[3])) + ' cm';
+    }
 }
 const areasquare = (side) => {
-    return side * side + ' cm^2';
+    if (side.length == 1) {
+        return (parseInt(side[0]) * parseInt(side[0])) + ' cm^2';
+    } else if (side.length > 1 && side.length < 4) {
+        alersweet1()
+    } else if (side[0] !== side[3]) {
+        alersweet2()
+    } else {
+        return (parseInt(side[0]) * parseInt(side[0])) + ' cm^2';
+    }
 }
-const tarea = (l1triangle, l2triangle, btriangle) => {
+const tperimeter = (l1triangle, l2triangle, btriangle) => {
     return l1triangle + l2triangle + btriangle + ' cm'
-};
-const tperimeter = (btriangle, height) => {
-    return (btriangle * height) / 2 + ' cm^2'
-};
-const noheight = (a, b, c) => {
-    const btriangle = c;
-    const s = (a + b + c) / 2;
-    const desfragmenting = (s * (s - a) * (s - b) * (s - c))
-    if (desfragmenting <= 0) {
-        alert('Its imposive get a root of negative number try other numbers');
+}; //perimeter
+const area3 = (a, b, c) => {
+    const l1 = (a + b + c);
+    const l2 = (-a + b + c);
+    const l3 = (a - b + c);
+    const l4 = (a + b - c);
+    const lr = l1 * l2 * l3 * l4;
+    const rlr = Math.sqrt(lr)
+    const calc = 0.25 * rlr;
+    if (calc < 1) {
+        const calcp = calc.toPrecision(2);
+        return calcp + ""
+    } else if (calc <= 9) {
+        const calcp = calc.toPrecision(3);
+        return calcp + ""
+    } else if (calc <= 99) {
+        const calcp = calc.toPrecision(4);
+        return calcp + ""
     } else {
-        const height = (Math.sqrt(desfragmenting)) / (btriangle) * 1 / 2;
-        const type = triangles(a, b, c)
-        return 'Heigth of your triangle is ' + height + ' and ' + type;
-    }
-};
-const theight = (lAg, lBg, lb) => {
-    if (lAg != lBg) {
-        alert('Its imposive get a root of negative number try other numbers');
-    }
-    if (!lAg, lBg, lb) {
-        return null
-    } else {
-        let lAp;
-        const lBp = lb / 2;
-        const lbp = lAg;
-        lAp = Math.sqrt((lbp ** 2) - (lBp ** 2))
-        return lAp;
+        const calcp = calc.toPrecision(6);
+        return calcp + ""
     }
 }
-const triangles = (a, b, c) => {
-    if (a == b && a != c || a == c && a != b) {
-        return 'Your triangle is Isoceles'
-    } else if (a == b && a == c) {
-        return 'Your triangle is Equilatero'
+const area2 = (a, b) => {
+    const ab = (b * a)
+    const dec = 0.5
+    const calc = ab * dec
+    if (calc <= 9) {
+        const calcp = calc.toPrecision(3);
+        return calcp + ""
+    } else if (calc <= 99) {
+        const calcp = calc.toPrecision(4);
+        return calcp + ""
     } else {
-        return 'Your triangle is Escaleno'
+        const calcp = calc.toPrecision(6);
+        return calcp + ""
     }
 }
+//area
+// const tperimeter = (btriangle, height) => {
+//     return (btriangle * height) / 2 + ' cm^2'
+// };
+// const noheight = (a, b, c) => {
+//     const btriangle = c;
+//     const s = (a + b + c) / 2;
+//     const desfragmenting = (s * (s - a) * (s - b) * (s - c))
+//     if (desfragmenting <= 0) {
+//         swal ('Its imposive get a root of negative number try other numbers',{
+//             buttons:['']
+//         });
+//     } else {
+//         const height = (Math.sqrt(desfragmenting)) / (btriangle) * 1 / 2;
+//         const type = triangles(a, b, c)
+//         return 'Heigth of your triangle is ' + height + ' and ' + type;
+//     }
+// };
+// const theight = (lAg, lBg, lb) => {
+// if (lAg != lBg) {
+//     swal('Its imposive get a root of negative number try other numbers',{
+//         buttons:['']
+//     });
+// }
+// if (!lAg, lBg, lb) {
+//     return null
+// } else {
+//         let lAp;
+//         const lBp = lb / 2;
+//         const lbp = lAg;
+//         lAp = Math.sqrt((lbp ** 2) - (lBp ** 2))
+//         return lAp;
+//     // }
+// }
+// const triangles = (a, b, c) => {
+//     if (a == b && a != c || a == c && a != b) {
+//         return 'Your triangle is Isoceles'
+//     } else if (a == b && a == c) {
+//         return 'Your triangle is Equilatero'
+//     } else {
+//         return 'Your triangle is Escaleno'
+//     }
+// }
 const isoceles = (a, b, c) => {
     return theight(a, b, c)
 }
@@ -72,7 +140,7 @@ const listeningC = () => {
     avoid(field)
 }
 const listeningT = () => {
-    const input = document.querySelector("#InputTrianglec");
+    const input = document.querySelector("#InputTrianglea");
     const button = document.querySelector(".trianglea");
     const buttonb = document.querySelector(".triangleb");
     const field = input.value.trim();
@@ -111,8 +179,10 @@ const textInsert = (result, id = '') => {
     p2.appendChild(text2)
 }
 const validate = (value, result, id = '', valueb) => {
-    if (value && valueb <= 0) {
-        return alert('Enter a value in all of fields')
+    if (parseInt(value[0]) <= 0) {
+        return swal("Only valid values", {
+            buttons: ["OK"]
+        });
     }
     textInsert(result, id)
 }
